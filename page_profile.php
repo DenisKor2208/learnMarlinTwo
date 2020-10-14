4ecd826099b4715a02eb200777410ac7ec12784c
DenisKor2208 <dkorotin@list.ru>
2020-10-14 13:40:44 +0700

    результат задания Профиль

--- page_profile.php
+++ page_profile.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+require "functions.php";
+$current_user_id = $_GET['id'];
+
+if (is_not_logged_in()) {
+    redirect_to("/page_login.php");
+}
+
+$user = get_user_by_email_or_id("two_person", null, $current_user_id);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +27,7 @@
 </head>
     <body class="mod-bg-1 mod-nav-link">
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-primary-gradient">
-            <a class="navbar-brand d-flex align-items-center fw-500" href="#"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
+            <a class="navbar-brand d-flex align-items-center fw-500" href="users.php"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarColor02">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item ">
@@ -23,19 +35,22 @@
                     </li>
                 </ul>
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Войти</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Выйти</a>
-                    </li>
+                    <?php if (is_not_logged_in()) :?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="page_login.php">Войти</a>
+                        </li>
+                    <?php else: ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="exit.php">Выйти</a>
+                        </li>
+                    <?php endif; ?>
                 </ul>
             </div>
         </nav>
         <main id="js-page-content" role="main" class="page-content mt-3">
             <div class="subheader">
                 <h1 class="subheader-title">
-                    <i class='subheader-icon fal fa-user'></i> Иван Иванов
+                    <i class='subheader-icon fal fa-user'></i> <?php echo $user['first_name'] ." ".$user['last_name']; ?>
                 </h1>
             </div>
             <div class="row">
@@ -45,19 +60,19 @@
                         <div class="row no-gutters row-grid">
                             <div class="col-12">
                                 <div class="d-flex flex-column align-items-center justify-content-center p-4">
-                                    <img src="img/demo/avatars/avatar-admin-lg.png" class="rounded-circle shadow-2 img-thumbnail" alt="">
+                                    <img src="img/avatar/<?php echo $user['img_avatar']; ?>" class="rounded-circle shadow-2 img-thumbnail" alt="">
                                     <h5 class="mb-0 fw-700 text-center mt-3">
-                                        Иван Иванов 
-                                        <small class="text-muted mb-0">Toronto, Canada</small>
+                                        <?php echo $user['first_name'] ." ".$user['last_name']; ?>
+                                        <small class="text-muted mb-0"><?php echo $user['company']; ?></small>
                                     </h5>
                                     <div class="mt-4 text-center demo">
-                                        <a href="javascript:void(0);" class="fs-xl" style="color:#C13584">
+                                        <a href="https://www.instagram.com/<?php echo $user['instagram']; ?>" class="fs-xl" style="color:#C13584">
                                             <i class="fab fa-instagram"></i>
                                         </a>
-                                        <a href="javascript:void(0);" class="fs-xl" style="color:#4680C2">
+                                        <a href="https://vk.com/<?php echo $user['vk']; ?>" class="fs-xl" style="color:#4680C2">
                                             <i class="fab fa-vk"></i>
                                         </a>
-                                        <a href="javascript:void(0);" class="fs-xl" style="color:#0088cc">
+                                        <a href="https://t.me/<?php echo $user['telegram']; ?>" class="fs-xl" style="color:#0088cc">
                                             <i class="fab fa-telegram"></i>
                                         </a>
                                     </div>
@@ -65,12 +80,12 @@
                             </div>
                             <div class="col-12">
                                 <div class="p-3 text-center">
-                                    <a href="tel:+13174562564" class="mt-1 d-block fs-sm fw-400 text-dark">
+                                    <a href="<?php echo $user['phone_number']; ?>" class="mt-1 d-block fs-sm fw-400 text-dark">
                                         <i class="fas fa-mobile-alt text-muted mr-2"></i> +1 317-456-2564</a>
-                                    <a href="mailto:oliver.kopyov@marlin.ru" class="mt-1 d-block fs-sm fw-400 text-dark">
+                                    <a href="mailto:<?php echo $user['email']; ?>" class="mt-1 d-block fs-sm fw-400 text-dark">
                                         <i class="fas fa-mouse-pointer text-muted mr-2"></i> oliver.kopyov@marlin.ru</a>
                                     <address class="fs-sm fw-400 mt-4 text-muted">
-                                        <i class="fas fa-map-pin mr-2"></i> Восточные Королевства, Штормград 15
+                                        <i class="fas fa-map-pin mr-2"></i> <?php echo $user['address']; ?>
                                     </address>
                                 </div>
                             </div>
